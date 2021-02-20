b75d9a9c78cb28fce64e052d5b03c717fd2ab17d
yessicsa <jessipandita2616@gmail.com>
2021-02-19 23:08:58 -0600

    factories y seeders

---
+++ database/factories/PostTagFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\post_tag;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PostTagFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = post_tag::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
